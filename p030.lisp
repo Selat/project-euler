f6145e8e0b1251975ca3ab9b4642604a9a9e56aa
selat <selatnick@gmail.com>
2015-04-13 20:32:20 +0300

    Solve problem 32

--- p030.lisp
+++ p030.lisp
@@ -1,18 +1,17 @@
+;; Just bruteforce precalc
+
+(defvar *digit-pows* (make-hash-table))
+(defvar *nums* (make-hash-table))
+(defconstant &maxn& 9999999)
+
 (defun get-digits (a)
   (if (> a 0) (cons (mod a 10) (get-digits (floor a 10)))))
 
-(defun find-max (a)
-  (loop for i in a maximize i))
-
-(defun is-pandigital (n a b c)
-  (let ((da (get-digits a))
-        (db (get-digits b))
-        (dc (get-digits c)))
-    (and (null (intersection da db))
-         (null (intersection da dc))
-         (null (intersection db dc))
-         (= (find-max (append da db dc)) n)
-         (= (length (append da db dc)) n))))
+(defun fill-digit-pows (n)
+  (clrhash *digit-pows*)
+  (loop for i from 0 to 9 do
+    (setf (gethash i *digit-pows*)
+          (pow i n))))
 
 (defun pow (a n)
   (let ((res 1))
@@ -20,10 +19,30 @@
       (setf res (* res a)))
     res))
 
-(defun solve (n)
-  (loop for i from 1 to (pow 10 (ceiling n 2)) sum
-    (loop for j from i to (pow 10 (ceiling n 2)) sum
-                                                 (if (is-pandigital n i j (* i j)) (progn ;;(format t "~A ~A~%" i j)
-                                                                                     (* i j)) 0))))
+(defun sum-digits (a)
+  (reduce #'+ (mapcar (lambda (a) (gethash a *digit-pows*))
+                      (get-digits a))))
 
-(print (solve (read)))
+(defun prefill-nums ()
+  (let ((res (make-hash-table)))
+    (loop for len from 3 to 6
+          for curres = nil
+          do
+      (fill-digit-pows len)
+      (loop for i from 1 to &maxn& do
+        (if (= (sum-digits i) i)
+            (setf curres (cons i curres))))
+      (setf (gethash len res) curres))
+    res))
+
+(defun fill-hash-nums ()
+  (setf (gethash 3 *nums*) '(407 371 370 153))
+  (setf (gethash 4 *nums*) '(9474 8208 1634))
+  (setf (gethash 5 *nums*) '(194979 93084 92727 54748 4151 4150))
+  (setf (gethash 6 *nums*) '(548834)))
+
+(defun solve (n)
+  (reduce #'+ (gethash n *nums*)))
+
+(fill-hash-nums)
+(format t "~A~%" (solve (read)))
